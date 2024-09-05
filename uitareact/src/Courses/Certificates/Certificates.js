f929cf66a7f04a2a4bfb08f7a3899e8d24571678
Dilawar Khan <Dilawarkhan03401417862@gmail.com>
2024-09-05 12:06:50 +0500

    Update

--- uitareact/src/Courses/Certificates/Certificates.js
+++ uitareact/src/Courses/Certificates/Certificates.js
@@ -1,105 +1,72 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { Link } from "react-router-dom";
-
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Certificate from './Certificate/Certificate';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
-
+import { Container, Row, Col } from 'react-bootstrap';
 import globalVar from '../../globalVar';
 
 export class Certificates extends Component {
-    static propTypes = {}
+    static propTypes = {
+        id: PropTypes.number.isRequired,
+        courseName: PropTypes.string
+    };
+
     constructor(props) {
-        super();
+        super(props);
         this.state = {
             certificatesArray: [],
-            courseId: window.location.pathname.split("/").pop(),
-            vendorId: props.id,
-            // courseId: 7,
+            vendorId: props.id
         };
-
-        // console.log("api", this.state.certificatesArray )
-        
     }
     
     componentDidMount() {
-        const { id } = this.props;
-        const { courseName } = this.props.courseName;
-        console.log( "courseName K", this.props.courseName)
-        this.loadAvailableExams(id);
+        const { vendorId } = this.state;
+        this.loadCertificates(vendorId);
     }
 
-    loadAvailableExams(vendorId) {
-        const { page } = this.state;
-        // console.log( "this.state", this.state)
+    loadCertificates(vendorId) {
         const requestOptions = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ vendorId: vendorId })
+            body: JSON.stringify({ vendorId })
         };
-        // Fetching data from FaceBook Jest Repo
-        fetch( globalVar.url + 'certificates', requestOptions )
+        fetch(globalVar.url + 'certificates', requestOptions)
             .then(res => res.json())
             .then(response => {
-                // console.log("RRRRRRRRR", response.data)
-                // const filteredCertificate = response.data.filter(e => {
-                //     return e.certificationId == this.state.vendorId
-                // });
-                this.setState(
-                    { certificatesArray: response.data },
-                    () => console.log("Certificates", this.state.certificatesArray)
-                );
-            }
-            )
+                this.setState({
+                    certificatesArray: response.data
+                });
+            })
             .catch(error => console.log(error));
     }
 
-
     render() {
-        const { certificatesArray } = this.state;
-
-        
-        // const { id } = this.props;
-        // console.log("this.props.id", id )
+        const { certificatesArray, vendorId } = this.state;
 
         return (
-            <>
-
-                {
-                    certificatesArray.length > 0 ?
-                        <>
-                            <section className="section section-courses inner-page">
-                                <Container>
-                                    {/* <h2 className="title mb-4">Available exam courses</h2> */}
-                                    <Row>
-
-                                        {certificatesArray.map((e, index) => (
-
-                                            <Col sm={6} lg={4} xl={3} key={e.certificationId}>
-                                                {e && (
-                                                    <Certificate
-                                                        key={index}
-                                                        vendorId={e.vendorId}
-                                                        certificationId={e.certificationId}
-                                                        certification_Name={e.certification_Name}
-                                                        status={e.status}
-                                                        detail={e.detail}
-                                                        instructor_id={e.instructor_id}
-
-                                                    />
-                                                )}
-                                            </Col>
-                                        ))}
-                                    </Row>
-
-                                </Container>
-                            </section>
-                        </>
-                        : ''
-                }
-            </>
-        )
+            <section className="section section-courses inner-page">
+                <Container>
+                    {certificatesArray.length > 0 ? (
+                        <Row>
+                            {certificatesArray.map((e) => (
+                                <Col sm={6} lg={4} xl={3} key={e.certificationId}>
+                                    <Certificate
+                                        certificationId={e.certificationId}
+                                        certification_Name={e.certification_Name}
+                                        vendorId={vendorId} // Pass vendorId here
+                                    />
+                                </Col>
+                            ))}
+                        </Row>
+                    ) : (
+                        <div className="alert alert-info">
+                            No certificates available.
+                        </div>
+                    )}
+                </Container>
+            </section>
+        );
     }
 }
 
-export default Certificates
+export default Certificates;
